c4eea8889dacc21c9ac19cfa585daebedce617e9
Louisa Lee <louisa.leeyh@gmail.com>
2017-06-17 17:05:04 +0800

    ok

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,21 @@
 	var incoming = document.getElementById('incoming');	
 	var resolved = document.getElementById('resolved');
-	var form = document.getElementById('form');
+	var menu = document.getElementById('menu');
 
 function showResolved() {
     incoming.style.display = 'none';
     resolved.style.display = 'block';
-    form.style.display = 'none';
+    menu.style.display = 'none';
 }
 
 function showIncoming() {
     resolved.style.display = 'none';
     incoming.style.display = 'block';
-    form.style.display = 'none';
+    menu.style.display = 'none';
 }
 
 function showForm() {
     resolved.style.display = 'none';
     incoming.style.display = 'none';
-    form.style.display = 'block';
+    menu.style.display = 'block';
 }
